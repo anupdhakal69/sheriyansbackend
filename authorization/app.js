ed30f21abd22e22ad7046b19881a5c64c9c8565b
Anup D <dhakalanup96@gmail.com>
2024-08-22 15:28:10 +0545

    hehe

--- authorization/app.js
+++ authorization/app.js
@@ -7,18 +7,43 @@
 
 const express = require('express');
 const app = express();
+const path = require('path');
 const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
+const usermodel = require('./models/user');
+const jwt = require('jsonwebtoken');
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
 
 
 app.get('/', (req, res) => {
-    bcrypt.genSalt(10, function(err, salt) {
-        bcrypt.hash("honolulu", salt, function(err, hash) {
-            console.log(hash);
-            
-        });
-    });
+   res.render('index');
 });
 
+app.post('/create', (req, res) => {
+    let { username, email, password, age } = req.body;
+
+    bcrypt.genSalt(10,(err,salt) => {
+        bcrypt.hash(password,salt, async(err,hash) => {
+
+            let createdUsers = await usermodel.create({
+                username,
+                email,
+                password: hash,
+                age
+        })
+
+        let token = jwt.sign({email},"secretkey");
+        res.cookie('token',token,);
+        res.send(createdUsers);
+    })
+
+    });
+})
 
 app.listen(3000, () => {
     console.log(`Server is running on port ${3000}`);
